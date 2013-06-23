1a7bbbce311135150ca447f992bcd6c02f0a464e
Xinyue Zhao <czhao@cellular.de>
2013-06-23 15:04:49 +0200

    Added DrawLayout(sidebar)

--- src/com/gmail/hasszhao/mininews/MainActivity.java
+++ src/com/gmail/hasszhao/mininews/MainActivity.java
@@ -2,9 +2,13 @@ package com.gmail.hasszhao.mininews;
 
 import uk.co.senab.actionbarpulltorefresh.extras.actionbarsherlock.PullToRefreshAttacher;
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshAttacher.OnRefreshListener;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -17,6 +21,7 @@ import com.gmail.hasszhao.mininews.utils.Prefs;
 public class MainActivity extends SherlockFragmentActivity {
 
 	private PullToRefreshAttacher mPullToRefreshAttacher;
+	private ActionBarDrawerToggle mDrawerToggle;
 
 
 	@Override
@@ -27,11 +32,78 @@ public class MainActivity extends SherlockFragmentActivity {
 
 
 	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initActionbar();
 		mPullToRefreshAttacher = new PullToRefreshAttacher(this);
 		showNewsListFragment();
+		createSidebar();
+	}
+
+
+	private void initActionbar() {
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+	}
+
+
+	private static class DrawerListener implements DrawerLayout.DrawerListener {
+
+		private final ActionBarDrawerToggle drawerToggle;
+
+
+		public DrawerListener(ActionBarDrawerToggle _drawerToggle) {
+			super();
+			drawerToggle = _drawerToggle;
+		}
+
+
+		@Override
+		public void onDrawerOpened(View drawerView) {
+			drawerToggle.onDrawerOpened(drawerView);
+		}
+
+
+		@Override
+		public void onDrawerClosed(View drawerView) {
+			drawerToggle.onDrawerClosed(drawerView);
+		}
+
+
+		@Override
+		public void onDrawerSlide(View drawerView, float slideOffset) {
+			drawerToggle.onDrawerSlide(drawerView, slideOffset);
+		}
+
+
+		@Override
+		public void onDrawerStateChanged(int newState) {
+			drawerToggle.onDrawerStateChanged(newState);
+		}
+	}
+
+
+	private void createSidebar() {
+		DrawerLayout sidebar = (DrawerLayout) findViewById(R.id.sidebar);
+		sidebar.setDrawerListener(new DrawerListener(mDrawerToggle = new ActionBarDrawerToggle(this, sidebar,
+				R.drawable.ic_drawer, -1, -1)));
+		sidebar.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 	}
 
 
